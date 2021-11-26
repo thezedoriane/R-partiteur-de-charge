e729b0d1725f0de1ebedea0ec572e22e3abe46ee
thezedoriane <72502637+thezedoriane@users.noreply.github.com>
2021-11-26 09:08:59 +0100

    Delete Notification.java

--- Notification.java
+++
@@ -1,8 +0,0 @@
-package repartiteur;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Notification extends Remote{
-	public void charge(Machine m,int load) throws RemoteException;
-}
